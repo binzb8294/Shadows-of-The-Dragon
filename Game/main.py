3106ef1166e3beb8a1ad4251bb199057516c114e
ThomasYebesiDev <ThomasYebesi@outlook.com>
2024-11-24 22:19:06 -0800

    version 0.4
    player can now move through maps

--- Game/main.py
+++ Game/main.py
@@ -14,18 +14,36 @@ class Game:
         pg.key.set_repeat(500, 100)
         self.load_data()
 
-        
+    
     def load_data(self):
         game_folder = path.dirname(__file__)
-        self.map_data =[]
-        with open(path.join(game_folder, 'Maps/map.txt'), 'rt') as f:
+        self.currentMap=0
+        self.map_list = []
+        self.map_data_L1R1 = []
+        self.map_data_L1R2 = []
+        self.map_data_L1R3 = []
+        with open(path.join(game_folder, 'Maps/L1R1.txt'), 'rt') as f:
             for line in f:
-                self.map_data.append(line)
-
+                self.map_data_L1R1.append(line)
+        self.map_list.append(self.map_data_L1R1)
+        with open(path.join(game_folder, 'Maps/L1R2.txt'), 'rt') as f:
+            for line in f:
+                self.map_data_L1R2.append(line)
+        self.map_list.append(self.map_data_L1R2)
+        with open(path.join(game_folder, 'Maps/L1R3.txt'), 'rt') as f:
+            for line in f:
+                self.map_data_L1R3.append(line)
+        self.map_list.append(self.map_data_L1R3)
     def new(self):
         # initialize all variables and do all the setup for a new game
+        self.load_data()
         self.all_sprites = pg.sprite.Group()
         self.walls = pg.sprite.Group()
+        self.tiles = pg.sprite.Group()
+        self.holes = pg.sprite.Group()
+        self.players = pg.sprite.Group()
+        self.player = Player(self,-1,-1)
+        self.numEnemies = 0
         self.createMap()
        
 
@@ -37,45 +55,61 @@ class Game:
             self.events()
             self.update()
             self.draw()
-            
+    def changeMap(self,mapChange):
+        self.currentMap += mapChange
+        print('After change')
+        print(self.currentMap)
     def createMap(self):
-         self.load_data()
-         XAdjust = (WIDTH/TILESIZE/2)-(len(self.map_data[0])/2)
-         YAdjust = (HEIGHT/TILESIZE/2)-(len(self.map_data)/2)
-         for row, tiles in enumerate(self.map_data):
+         print('born')
+         
+         self.resetMap()
+         XAdjust = (WIDTH/TILESIZE/2)-(len(self.map_list[0][self.currentMap])/2)
+         YAdjust = (HEIGHT/TILESIZE/2)-(len(self.map_list[self.currentMap])/2)
+         for row, tiles in enumerate(self.map_list[self.currentMap]):
              for col, tile in enumerate(tiles):
                  if tile == ".":
-                     floorTile(self,col+YAdjust,row+XAdjust)
+                     floorTile(self,col+XAdjust,row+YAdjust)
                  elif tile == 'P':
-                     floorTile(self,col+YAdjust,row+XAdjust)
+                     Hole(self,col+XAdjust,row+YAdjust,-1)
+                 elif tile == 'E':
+                     floorTile(self,col+XAdjust,row+YAdjust)
+                 elif tile == 'H':
+                     Hole(self,col+XAdjust,row+YAdjust,1)
                  elif tile == 'M':
-                     Wall(self,col+YAdjust,row+XAdjust,tile)
+                     Wall(self,col+XAdjust,row+YAdjust,tile)
                  elif tile == '\n':
                      pass
                  else:
-                     Wall(self,col+YAdjust,row+XAdjust,tile)
+                     Wall(self,col+XAdjust,row+YAdjust,tile)
 
 
-         for row, tiles in enumerate(self.map_data):
+         for row, tiles in enumerate(self.map_list[self.currentMap]):
              for col, tile in enumerate(tiles):
                  if tile == 'P':
-                     self.player = Player(self,col+YAdjust,row+XAdjust)
+                      self.player=Player(self,col+XAdjust,row+YAdjust-1,self.player.level,self.player.exp,self.player.health)
+                      self.killClones(self.player)
+                     
     def quit(self):
         pg.quit()
         sys.exit()
-
+    def killClones(self,myPlayer):
+        for Player in self.players:
+            if Player != myPlayer:
+                Player.kill()
+    def resetMap(self):
+        for tile in self.tiles:
+            tile.kill()
     def update(self):
         # update portion of the game loop
         self.all_sprites.update()
-
-    
-
+     
     def draw(self):
         self.screen.fill(BGCOLOR)
     
         self.all_sprites.draw(self.screen)
+        
         pg.display.flip()
-
+    
     def events(self):
         # catch all events here
         for event in pg.event.get():
@@ -92,7 +126,7 @@ class Game:
                     self.player.move(dy=-1)
                 if event.key == pg.K_DOWN:
                     self.player.move(dy=1)
-
+                
     def show_start_screen(self):
         pass
 
--- Game/sprites.py
+++ Game/sprites.py
@@ -3,33 +3,52 @@ from settings import *
 from pygame.locals import *
 
 class Player(pg.sprite.Sprite):
-    def __init__(self, game, x, y):
+    def __init__(self, game, x, y, level=1, exp=0, health=10):
         self.file = 'Assets/Knight.png'
         self.image = pg.image.load(self.file)
         self.groups = game.all_sprites
+        
         self.rect = self.image.get_rect()
         pg.sprite.Sprite.__init__(self, self.groups)
         self.game = game
         
         self.game.screen.blit(self.image,self.rect)
-  
+        self.level=level
+        self.exp=exp
+        self.health=health
         self.x = x
         self.y = y
-     
+        
     
     def move(self, dx=0, dy=0):
         if not self.collide_with_walls(dx, dy):
             self.x += dx
             self.y += dy
-          
+            self.enterHole(dx,dy)
+            print('map')
+            print(self.game.currentMap)
             
-
+    def enterHole(self, dx=0,dy=0):
+        for Hole in self.game.holes:
+            if Hole.x == self.x and Hole.y-1 == self.y:
+                if Hole.Type == 1:
+                    if self.game.currentMap != len(self.game.map_list)-1:
+                        self.game.changeMap(Hole.Type)
+                        self.game.createMap()
+                if Hole.Type == -1:
+                    if self.game.currentMap != 0:
+                        self.game.changeMap(Hole.Type)
+                        self.game.createMap()
+    def setPosition(self,sx=0,sy=0):        
+        self.x=sx
+        self.y=sy
+        self.game.screen.blit(self.image,self.rect)
     def collide_with_walls(self, dx=0, dy=0):
         for wall in self.game.walls:
             if wall.x == self.x + dx and wall.y == self.y + dy+1:
                 return True
         print("tilecords")
-        print(self.x+dx,self.y+dy)
+        print(self.x,self.y)
         return False
         self.groups = game.all_sprites
         pg.sprite.Sprite.__init__(self, self.groups)
@@ -47,7 +66,7 @@ class floorTile(pg.sprite.Sprite):
     def __init__(self,game,x,y):
         self.file = 'Assets/Floor Tile.png'
         self.image = pg.image.load(self.file)
-        self.groups = game.all_sprites
+        self.groups = game.all_sprites, game.tiles
         pg.sprite.Sprite.__init__(self, self.groups)
         self.game = game
        
@@ -56,11 +75,11 @@ class floorTile(pg.sprite.Sprite):
         self.y = y
         self.rect.x = x * TILESIZE
         self.rect.y = y * TILESIZE
-        self.game.screen.blit(self.image,self.rect)
+        #self.game.screen.blit(self.image,self.rect)
 
 class Wall(pg.sprite.Sprite):
     def __init__(self, game, x, y, wallType):
-        self.groups = game.all_sprites, game.walls
+        self.groups = game.all_sprites, game.walls, game.tiles
         pg.sprite.Sprite.__init__(self, self.groups)
         self.game = game
         self.image = pg.Surface((TILESIZE, TILESIZE))
@@ -91,6 +110,21 @@ class Wall(pg.sprite.Sprite):
         self.y = y
         self.rect.x = x * TILESIZE
         self.rect.y = y * TILESIZE
-        self.game.screen.blit(self.image,self.rect)
+        #self.game.screen.blit(self.image,self.rect)
+class Hole(pg.sprite.Sprite):
+    def __init__(self, game, x, y, Type):
+          self.groups = game.all_sprites, game.holes, game.tiles
+          pg.sprite.Sprite.__init__(self, self.groups)
+          self.game = game
+          self.image = pg.image.load('Assets/Hole.png')
+          self.rect = self.image.get_rect()
+          self.Type=Type
+          self.x = x
+          self.y = y
+          print('holecords')
+          print(x)
+          print(y)
+          self.rect.x = x * TILESIZE
+          self.rect.y = y * TILESIZE
 #class tile(self,TileName,x,y):
   
